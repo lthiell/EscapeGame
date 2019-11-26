642178cb3f7facabdd8c6bf549bd05e4045a03c5
Lukas Thiell <lukasthiell@icloud.com>
2019-11-26 15:43:19 +0100

    Added Library Scripts to repo

---
+++ EscapeGame/Assets/Scripts/Library/Door.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private Animator animator;
+    private ObjectHider objectHider = ObjectHider.GetSingleton();
+    // Objekte, die ausgeblendet sein müssen, wenn die Tür geschlossen ist (--> der Flur)
+    public List<GameObject> container = new List<GameObject>();
+     
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        foreach (GameObject go in container)
+        {
+            objectHider.HideGameObject(go);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        animator.SetTrigger("OpenDoor");
+        foreach (GameObject go in container)
+        {
+            objectHider.ShowGameObject(go);
+        }
+    }
+
+}
